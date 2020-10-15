454cca96f83fd8cc589d2d4f7a9402c9dc23b94f
YaoAnn <742446272@qq.com>
2020-10-15 17:33:52 +0800

    自己构建前端

---
+++ src/main/java/com/yaohuaxiang/config/LoginSecurity.java
@@ -0,0 +1,7 @@
+package com.yaohuaxiang.config;
+
+/**
+ * @author yaohuaxiang
+ * @create 2020/10/15 - 14:29
+ */
+
---
+++ src/main/java/com/yaohuaxiang/controller/CommonController.java
@@ -0,0 +1,17 @@
+package com.yaohuaxiang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author yaohuaxiang
+ * @create 2020/10/15 - 14:48
+ */
+@Controller
+public class CommonController {
+
+   /* @GetMapping()
+    public String toindex(){
+        return "redirect:index.html";
+    }*/
+}
